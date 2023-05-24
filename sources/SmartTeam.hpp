7c80cdc675438eef7eafcad37b311fc99b026c97
Hadar Bitan <86705118+HadarBitan@users.noreply.github.com>
2023-05-24 11:52:50 +0300

    Update SmartTeam.hpp

--- sources/SmartTeam.hpp
+++ sources/SmartTeam.hpp
@@ -6,6 +6,8 @@ namespace ariel
 {
     class SmartTeam: public Team
     {
+         //in Smart Team the method is to go over the ninjas first because the hit the most points
+        //in the ninjas we go over the fastes ninjas first in case the ninja is far from the target
         public:
             SmartTeam(Character *leader);
             void attack(Team *enemy) override;
